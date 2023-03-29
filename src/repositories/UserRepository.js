0ece776bd1170492aef41e0c858d6fdb9596ec16
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-03-28 21:00:05 -0300

    feat(Add): basic estructure of user's repository

---
+++ src/repositories/UserRepository.js
@@ -0,0 +1,23 @@
+class UserRepository {
+  async create() {
+
+  }
+
+  async list() {
+
+  }
+
+  async show() {
+
+  }
+
+  async update() {
+    
+  }
+
+  async remove() {
+
+  }
+}
+
+module.exports = new UserRepository();
