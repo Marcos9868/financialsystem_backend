1f9ad58010bb9caf94c3a1597115535706cb3a79
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-04-04 20:20:31 -0300

    feat(Add): createUser and listUsers

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,10 +1,23 @@
-class UserController {
-  async createUser() {
+const User = require('../models/User');
+const userRepository = require('../repositories/UserRepository');
+const userView = require('../views/UserView');
 
+class UserController {
+  async createUser(req, res) {
+    const { name, email, password, documentNumber } = req.body;
+    const id = new Date().getTime();
+
+    const newUser = new User(id, name, email, password, documentNumber);
+
+    await userRepository.create(newUser);
+
+    return userView.renderSuccess(res, { message: 'Cadastrado com sucesso' });
   }
 
-  async listUsers() {
+  async listUsers(res) {
+    const users = await userRepository.list();
 
+    return userView.renderSuccess(res, users);
   }
 
   async showUser() {
