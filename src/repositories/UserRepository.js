88b8c272de5fa46bd1067a7a6b7e39487d8d97bf
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-04-04 20:27:54 -0300

    feat(Add): create and list database scripts

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -1,10 +1,29 @@
 class UserRepository {
   async create() {
+    const script = `
+      INSERT INTO users
+      (name, email, password, documentNumber, status)
+      VALUES 
+      ($1, $2, $3, $4, $5)
+      RETURNING id
+    `;
 
+    const values = [];
+
+    const { rows } = await db.query(script, values);
+    const [ newUser ] = rows;
+
+    return newUser.id;
   }
 
   async list() {
+    const query = `
+      SELECT name, email, password FROM users
+    `;
 
+    const { rows } = await db.query(query)
+
+    return rows || [];
   }
 
   async show() {
