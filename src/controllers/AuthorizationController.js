de119e91b3bb7cb999e3b37e0b710ea5647b3816
Taiwan9 <clownser1994@gmail.com>
2023-05-19 14:22:35 -0300

    AuthorizationControllers update

--- src/controllers/AuthorizationController.js
+++ src/controllers/AuthorizationController.js
@@ -1,12 +1,68 @@
+
 class AuthorizationController {
   async loginUser(req, res, next) {
     const { email, password } = req.body;
 
+    try {
+
+      const user = await User.findOne({ email });
+  
+      if (!user) {
+
+        return res.status(404).json({ message: 'Usuário não encontrado' });
+      }
+  
+
+      const isPasswordValid = await user.comparePassword(password);
+  
+      if (!isPasswordValid) {
+
+        return res.status(401).json({ message: 'Senha inválida' });
+      }
+  
+      const token = generateAuthToken(user);
+  
+
+      return res.status(200).json({ token });
+    } catch (error) {
+
+      return next(error);
+    }
     
   }
 
   async validateUser(req, res, next) {
-    
+    const { token } = req.body;
+
+    try {
+
+      if (!token) {
+        return res.status(400).json({ message: 'Token de autenticação não fornecido' });
+      }
+  
+
+      const decodedToken = verifyAuthToken(token);
+  
+      if (!decodedToken) {
+        return res.status(401).json({ message: 'Token de autenticação inválido' });
+      }
+  
+
+      const user = await User.findById(decodedToken.userId);
+  
+      if (!user) {
+        return res.status(404).json({ message: 'Usuário não encontrado' });
+      }
+  
+
+      req.user = user;
+  
+
+      next();
+    } catch (error) {
+
+      return next(error);
+    }
   }
 }
 
